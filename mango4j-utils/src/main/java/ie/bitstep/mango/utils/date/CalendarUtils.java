bf406404886751d1c38078f9b9dac606f9f650c6
Ashley-Waldron <ashley.waldron@gmail.com>
2026-07-19 13:55:51 +0100

    Added missing another sonar squid to suppress and marked deprecated  classes for removal

--- mango4j-utils/src/main/java/ie/bitstep/mango/utils/date/CalendarUtils.java
+++ mango4j-utils/src/main/java/ie/bitstep/mango/utils/date/CalendarUtils.java
@@ -6,8 +6,8 @@ import java.util.TimeZone;
 /**
  * @deprecated Uses old Java date API.
  */
-@Deprecated(since = "1.1.0")
-@SuppressWarnings({"java:S2143", "java:S1133"}) // Suppress Sonar warnings to keep working legacy Calendar code
+@Deprecated(since = "1.1.0", forRemoval = true)
+@SuppressWarnings({"java:S2143", "java:S2972", "java:S1133"}) // Suppress Sonar warnings to keep working legacy Calendar code
 public class CalendarUtils {
 	/**
 	 * Prevents instantiation.
--- mango4j-utils/src/main/java/ie/bitstep/mango/utils/date/DateUtils.java
+++ mango4j-utils/src/main/java/ie/bitstep/mango/utils/date/DateUtils.java
@@ -15,8 +15,8 @@ import java.util.TimeZone;
  * @deprecated Uses old Java date API.
  * Utility class for working with dates and times.
  */
-@Deprecated(since = "1.1.0")
-@SuppressWarnings({"java:S2143", "java:S1133"}) // Suppress Sonar warnings to keep working legacy Calendar code
+@Deprecated(since = "1.1.0", forRemoval = true)
+@SuppressWarnings({"java:S2143", "java:S2972", "java:S1133"}) // Suppress Sonar warnings to keep working legacy Calendar code
 public class DateUtils {
 
 	private static final String DATE_FORMAT = "yyyy-MM-dd";
